5395bb5e562d4357268637e9e1ced5069c169167
Kaushal Dokania <kaushaldokania@gmail.com>
2022-03-02 20:55:30 +0530

    Initial commit

---
+++ src/main/java/org/kaushal/covid19tracker/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package org.kaushal.covid19tracker.controllers;
+
+import org.kaushal.covid19tracker.models.LocationStats;
+import org.kaushal.covid19tracker.services.Covid19DataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    Covid19DataService covid19DataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = covid19DataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        int totalNewCases = allStats.stream().mapToInt(LocationStats::getDiffFromPrevDay).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+        return "home";
+    }
+
+}
---
+++ src/test/java/org/kaushal/covid19tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaushal.covid19tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
